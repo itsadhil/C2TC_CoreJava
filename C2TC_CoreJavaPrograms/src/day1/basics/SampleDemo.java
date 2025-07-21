cd4cd0f8cf4fc97ec8d8b9e62e4201a7ba1e88f9
itsadhil <adhilquazi@gmail.com>
2025-07-21 14:45:12 +0530

    Fix

--- C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
@@ -1,11 +1,23 @@
 package day1.basics;
 
 public class SampleDemo {
+	
+	public static void main() {
+		System.out.println("Welcome from Main 1");
+	}
+	
+	public static void main(char c[]) {
+		System.out.println("Welcome from Main 2");
+	}
+	
+	public void main(String ar) {
+		System.out.println("Welcome from Main 3");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to C2TC........");
 		System.out.println("New Lines");
-
 	}
+		
 
 }
