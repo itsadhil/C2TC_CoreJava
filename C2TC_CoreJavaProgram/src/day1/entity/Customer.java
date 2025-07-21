b75c6266c8241853707341f5f0c7c1e8d0e7c86e
itsadhil <adhilquazi@gmail.com>
2025-07-21 16:05:05 +0530

    Learned the Getter and Setter and toString

--- C2TC_CoreJavaProgram/src/day1/entity/Customer.java
+++ C2TC_CoreJavaProgram/src/day1/entity/Customer.java
@@ -1,8 +1,40 @@
 package day1.entity;
 
 public class Customer {
-	int cid;
-	String name;
-	String city;
+	private int cid;
+	private String name;
+	private String city;
+	
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	
+	public int getCid() {
+		return this.cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [cid=" + cid + ", name=" + name + ", city=" + city + ", getCid()=" + getCid() + ", getName()="
+				+ getName() + ", getCity()=" + getCity() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
 
 }
--- C2TC_CoreJavaProgram/src/day1/entity/CustomerDemo.java
+++ C2TC_CoreJavaProgram/src/day1/entity/CustomerDemo.java
@@ -5,23 +5,17 @@ public class CustomerDemo {
 	public static void main(String[] args) {
 		//Customer 1
 		Customer c1 = new Customer();
-		c1.cid = 101;
-		c1.name = "raja";
-		c1.city = "Puducherry";
-		System.out.println("Customer id:" + c1.cid);
-		System.out.println("Customer name:" + c1.name);
-		System.out.println("Customer city :" + c1.city);
+		c1.setCid(101);
+		c1.setName("raja");
+		c1.setCity("Puducherry");
+		System.out.println(c1);
 		
-		//Customer 2
 		Customer c2 = new Customer();
-		c2.cid = 102;
-		c2.name = "Om Prakash";
-		c2.city = "Puducherry";
-		System.out.println("Customer id:" + c2.cid);
-		System.out.println("Customer name:" + c2.name);
-		System.out.println("Customer city :" + c2.city);
-		
-
+		c2.setCid(102);
+		c2.setName("Om Prakash");
+		c2.setCity("Puducherry");
+		System.out.println(c2);
 	}
-
+	
+	
 }
