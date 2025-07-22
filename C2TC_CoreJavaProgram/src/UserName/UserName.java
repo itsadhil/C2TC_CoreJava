ceddddf3741fe80d65ed73833a070944a8dacc6d
itsadhil <adhilquazi@gmail.com>
2025-07-22 14:08:49 +0530

    Next and Next Line is Learnt

---
+++ C2TC_CoreJavaProgram/src/UserName/UserName.java
@@ -0,0 +1,11 @@
+package UserName;
+import java.util.*;
+public class UserName {
+	public static void main(String args[])
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String n = input.nextLine();
+		System.out.println("Hello " + n);
+	}
+}
